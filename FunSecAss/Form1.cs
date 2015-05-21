a0f329909a36582f62316320a9fab6f838025f17
SasaDenda <Sasa.Denda@student.uts.edu.au>
2015-05-21 14:52:40 +1000

    authentication has been set up, references a list of users and passwords to check validity.

--- FunSecAss/Form1.cs
+++ FunSecAss/Form1.cs
@@ -12,9 +12,12 @@ namespace FunSecAss
 {
     public partial class Form1 : Form
     {
+        private AuthServer myAuthServer;
+        
         public Form1()
         {
             InitializeComponent();
+            myAuthServer = new AuthServer();
         }
 
         private void requestAuthenticationButton_Click(object sender, EventArgs e)
@@ -22,7 +25,32 @@ namespace FunSecAss
             string userName = userNameTextBox.Text;
             string password = passwordTextBox.Text;
 
-            ASreplyTextBox.Text = password;
+            using (System.IO.StreamWriter AuthRequest = new System.IO.StreamWriter(@"AuthRequest.txt"))
+            {
+                AuthRequest.WriteLine("Username: " + userName);
+                AuthRequest.WriteLine("Password: " + password);
+                AuthRequest.Close();
+            }
+            
+            switch(myAuthServer.authenticate())
+            {
+                case 0:
+                    ASreplyTextBox.Text = "Success";
+                    break;
+                case -1:
+                    ASreplyTextBox.Text = "User Name doesn't exist";
+                    break;
+                case -2:
+                    ASreplyTextBox.Text = "Incorrect Password";
+                    break;
+                case -3:
+                    ASreplyTextBox.Text = "File corrupted";
+                    break;
+            }
+
+            //ASreplyTextBox.Text = myAuthServer.authenticate().ToString();
+
+            //ASreplyTextBox.Text = password;
         }
 
         private void hidePasswordCheckbox_CheckedChanged(object sender, EventArgs e)
